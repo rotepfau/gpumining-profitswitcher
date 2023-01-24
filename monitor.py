2b264c53e37b8ed0f08dfe1d752673ac76e27601
rotepfau <rotepfau@gmail.com>
2023-01-24 12:18:49 -0400

    :construction: added argv support
    first and unique argv implemented. Parameter in days

--- monitor.py
+++ monitor.py
@@ -1,10 +1,23 @@
 import sqlite3
+import sys
+from time import time
+
+
+def getTimeFrame():
+    if len(sys.argv) != 2:
+        raise Exception('Missed timeframe parameter in days')
+    now = int(time())
+    days_selected = int(sys.argv[1]) * 60 * 60 * 24
+    return str(now - days_selected)
+
 
 if __name__ == '__main__':
+    timeframe = getTimeFrame()
     statistic = dict()
     con = sqlite3.connect('/home/user/gpumining-profitswitcher/database.db')
     cur = con.cursor()
-    res = cur.execute('select * from data').fetchall()
+    res = cur.execute('SELECT * FROM data WHERE date >= ?',
+                      timeframe).fetchall()
     for i, data in enumerate(res[:-1]):
         timelength = res[i + 1][1] - data[1]
         if data[2] not in statistic:
